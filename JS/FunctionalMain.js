d87a4eee0e285ee5022a440b7db83ebe873acd4e
Robson Luan <robsonluan273@hotmail.com>
2022-05-27 15:41:34 -0300

    added some functions

--- JS/FunctionalMain.js
+++ JS/FunctionalMain.js
@@ -1,25 +1,22 @@
 import { db, fd, database } from '../../MatriculaBot-Node.js/IA/DataKeeper.js'
 
 class UserData{
-    constructor(){
+    constructor(){}
 
+    async refreshUsersData(){
+        let user = (await db.request(`select r.*, c.cursonome from registro as r join cursos as c on 
+            r.curso = c.id;`))[0]
+        this.users = {ready: user.filter(i => i.finished === 1), unfin: user.filter(i => i.finished === 0)}
     }
 
-    static async getUsersData(){
-        return (await db.request(`select r.*, c.nome from registro as r join cursos as c on r.curso = c.id where finished = '1';`))[0]
-    }
-
-    static async getUserSubj(num){
-        let user = await fd.getUser(num)
-        console.log(user)
+    async getUserSubj(num){
+        await this.refreshUsersData()
+        let user = this.users.ready.find(i => i.numero === num)
         return (await db.request(`select u.discId, d.nome from user_${db.cursos[user.curso]} as u join disc_${db.cursos[user.curso]} as d on 
             u.discId = d.id where matricula = '${user.matricula}';`))[0]
     }
-    /*
-    static async getUserSubj(user){
-        return (await db.request(`select * from user_${db.cursos[user.curso]}
-            where matricula = '${user.matricula}';`))
-    }*/
+
+    
 }
 
 export {UserData}
--- JS/Main.js
+++ JS/Main.js
@@ -1,9 +1,29 @@
 import { UserData } from './FunctionalMain.js'
 
+const users = new UserData()
+
+async function setFinishedUsers(){
+    await users.refreshUsersData()
+    users.ready.forEach(i => {
+        //Aqui os dados estão dentro do i, basta chamar as funções de colocar o texto nos campos específicos.
+        //i = {matricula, numero, nome, email, cursonome, turma, cpf}
+        //Ex: Label.setHTML = (`${i.matricula} ${i.numero}`)
+    });
+}
+
+async function setUnfinishedUsers(){
+    await users.refreshUsersData()
+    users.unfin.forEach(i => {
+        //Aqui os dados estão dentro do i, basta chamar as funções de colocar o texto nos campos específicos.
+        //i = {matricula, numero, nome, email, cursonome, turma, cpf}
+        //Ex: Label.setHTML = (`${i.matricula} ${i.numero}`)
+    });
+}
+
 async function test(){
-    let users = await UserData.getUsersData()
-    console.log(users)
-    console.log((await UserData.getUserSubj(users[0].numero)))
+    await users.refreshUsersData()
+    console.log(users.users)
+    console.log((await users.getUserSubj(users.users.ready[3].numero)))
 }
 
 test()
