82bc571538d15047b418fca8906a7be1ccd14855
JoseWillians <jwillians@acad.ifma.edu.br>
2022-05-31 00:21:12 -0300

    adição de tabelas

---
+++ JS/botjs.js
@@ -0,0 +1,11 @@
+const cont = document.getElementById('button-dados');
+const testse = document.getElementById('testse')
+
+function ocultar (){
+    cont = 1;
+    if(cont ==1 ){
+        testse = 5;
+        console.log(testse);
+        console.log('funcionando');
+    }
+}
