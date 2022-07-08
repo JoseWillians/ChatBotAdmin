940b6c922564f12b1737c06b1a581c2bd9428015
JoseWillians <jwillians@acad.ifma.edu.br>
2022-07-08 15:27:33 -0300

    Iniciação com REST API

--- JS/botjs.js
+++ JS/botjs.js
@@ -1,10 +1,9 @@
-const cont = document.getElementById('button-dados');
-const testse = document.getElementById('testse')
+let cont = document.getElementById('button-dados');
+let testse = document.getElementById('testse')
 
 function ocultar (){
     cont = 1;
     if(cont ==1 ){
-        testse = 5;
         console.log(testse);
         console.log('funcionando');
     }
